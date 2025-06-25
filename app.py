b7db2242125845cf42ad31f08fdd35b4b13e49d7
Sandhya <sandhyak23901@gmail.com>
2025-06-25 11:16:09 +0530

    fix: render port binding to 0.0.0.0

--- app.py
+++ app.py
@@ -602,4 +602,6 @@ def complete2():
 
 
 if __name__=='__main__':
-    app.run(debug=True)      #server activate
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+    
